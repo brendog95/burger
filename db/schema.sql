4eacd0d93fbf86664466eba1322dbe8e3d884fed
Brenden Forbis <brenden.forbis@outlook.com>
2020-10-03 20:15:40 -0400

    Functioning Burger App

---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+ CREATE DATABASE burgers_db;
+USE burgers_db;
+CREATE TABLE burgers (
+    id int auto_increment,
+    burger_name varchar(30),
+    devoured boolean DEFAULT False
+)
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Black bean burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("California burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Fiesta burger", false);
